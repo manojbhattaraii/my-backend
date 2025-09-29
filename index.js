cfa1b6d26dfa1e194db4b6f278e1a7443579343a
Manoj Bhattarai <manojbhattarai734@gmail.com>
2025-09-29 13:20:53 +0545

    Update index.js
    up

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 // Enable CORS
 app.use(cors({
-  origin: "http://127.0.0.1:5500",
+  origin: "https://manojbhattarai7.com.np/",
   credentials: true
 }));
 
